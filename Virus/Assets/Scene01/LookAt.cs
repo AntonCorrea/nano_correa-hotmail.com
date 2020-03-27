1add3dcb5e30eaa3717b40c9e759cf5599e74232
Nano <nano_correa@hotmail.com>
2020-03-27 04:07:14 -0300

    add LookAt.cs

---
+++ Virus/Assets/Scene01/LookAt.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAt : MonoBehaviour
+{
+    //public GameObject target;
+    private float angle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        angle = GetComponent<AngleSprite>().angle;
+        //transform.position += new Vector3(0,angle - 180,0);
+        transform.rotation = Quaternion.Euler(-42.72f,angle-180f,0f);
+    }
+}
